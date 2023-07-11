4d566b215a5af313bd5c431be1a2ec6d4ae0020a
Pranav Chauhan <83133554+PranavChauhan22@users.noreply.github.com>
2023-07-11 15:56:05 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter());
+  
+  // Enable CORS
+  app.use(cors());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
